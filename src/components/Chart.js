2e0a807bab0c87300f818ba1f8a5390bfaf0e64e
Henok Mekonnen <henatan99@yahoo.com>
2022-06-03 06:11:35 -0400

    Add horizontal rules, y-axis labels, and styling

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -6,6 +6,9 @@ const Chart = (props) => {
     const dataLength = data.length;
     const numericData = data.map(val => parseFloat(val));
     const maxHeight = Math.max(...numericData);
+    const dividerGap = 20;
+    const dividers = Math.ceil(maxHeight / dividerGap);
+    const chartHeight = dividers * dividerGap;
 
     return (
         <div className='chart'>
@@ -19,22 +22,29 @@ const Chart = (props) => {
                                 height: '100%',
                                 marginLeft: `${100/4/dataLength}%`,
                                 marginRight: `${100/4/dataLength}%`,
-                                // backgroundColor: "red"
                             }}
                             key = {ind}
                         >
                             <span
                                 style={{
                                     width: '100%',
-                                    height: `${val/maxHeight * 100}%`,
+                                    height: `${val/chartHeight * 100}%`,
                                     backgroundColor: "blue",
                                 }}
-                            />
+                            >
+                                <span>{val}</span>
+                            </span>
                         </div>
                     )
                 })
             }
+            {
+                [...Array(dividers + 1).keys()].map(divider => {
+                    return <span style={{width: '100%', height: 5, position: 'absolute', left: 0, bottom: `${divider * dividerGap / chartHeight * 100}%`}}><p>{divider * dividerGap}</p><hr></hr></span>
+                })
+            }
         </div>
     )
-} 
+}
+
 export default Chart;
